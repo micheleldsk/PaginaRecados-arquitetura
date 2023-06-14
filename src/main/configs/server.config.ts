4dfec0614d3ddfa370edd27e99011fc85580d1a5
micheleldsk <micheleldsk@gmail.com>
2023-06-14 16:29:00 -0300

    changed message api

--- src/main/configs/server.config.ts
+++ src/main/configs/server.config.ts
@@ -9,7 +9,10 @@ export const createServer = () => {
 
 	app.use(express.json());
 
-	app.get("/", (Req: Request, Res: Response) => Res.status(200).send(`<h1>Minha aplicação funfou!</h1>`))
+	app.get("/", (req: Request, res: Response) => 
+	res.status(200).send(`<h1>Bem vindo a minha aplicação!</h1>
+						  <h3>Refatoração afim de aplicar a arquitetura proposta 
+						  no módulo de Arquitetura de Software - Junho-2023</h3>`))
 
 	makeRoutes(app);
 
