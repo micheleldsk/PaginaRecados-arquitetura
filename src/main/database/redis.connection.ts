9ab1aebbcd222ff6a54f15b44f5c505ad2ceb013
micheleldsk <micheleldsk@gmail.com>
2023-06-14 15:52:00 -0300

    add render.yaml

--- src/main/database/redis.connection.ts
+++ src/main/database/redis.connection.ts
@@ -6,11 +6,11 @@ export class RedisConnection {
 
 	public static async connect(): Promise<void> {
 		if (!this._connection) {
-			this._connection = new Redis({
-				host: process.env.REDIS_HOST,
-				port: Number(process.env.REDIS_PORT),
-				username: process.env.REDIS_USERNAME,
-				password: process.env.REDIS_PASSWORD,
+			this._connection = new Redis({				
+				path: process.env.REDIS_URL
+				// port: Number(process.env.REDIS_PORT),
+				// username: process.env.REDIS_USERNAME,
+				// password: process.env.REDIS_PASSWORD,
 			});
 		}
 	}
